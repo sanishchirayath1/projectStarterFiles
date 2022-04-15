90f556636962581adbcf0be4f3f032a1095b9649
sanishchirayath1 <sanishchirayath@gmail.com>
2022-04-15 20:09:03 +0530

    project starter setup

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,14 +1,15 @@
-import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+import "@testing-library/jest-dom";
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
+    const heading = screen.getByRole("heading", {
+      name: /hello world/i,
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(heading).toBeInTheDocument();
+  });
+});
